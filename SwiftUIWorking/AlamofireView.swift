ff78e1be34c61f935d47a405e14df68b83d4faf4
ceyhun7741 <ceyhunyalcin01@gmail.com>
2024-08-13 14:12:30 +0300

    add alamofire

---
+++ SwiftUIWorking/AlamofireView.swift
@@ -0,0 +1,18 @@
+//
+//  AlamofireView.swift
+//  SwiftUIWorking
+//
+//  Created by Yasin on 13.08.2024.
+//
+
+import SwiftUI
+
+struct AlamofireView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AlamofireView()
+}
