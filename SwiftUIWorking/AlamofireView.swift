d36b4c3e51b07835d63a1625dfd7f6e274422130
ceyhun7741 <ceyhunyalcin01@gmail.com>
2024-12-09 11:02:38 +0300

    son denemeler

--- SwiftUIWorking/AlamofireView.swift
+++ SwiftUIWorking/AlamofireView.swift
@@ -6,10 +6,36 @@
 //
 
 import SwiftUI
+import Alamofire
 
 struct AlamofireView: View {
+    @State private var responseData: String = "Loading..."
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text(responseData)
+                .padding()
+                .onTapGesture{
+                    fetchData()
+                }
+        }
+    }
+    
+    func fetchData() {
+        let url = "https://jsonplaceholder.typicode.com/posts/1"
+        
+        AF.request(url)
+            .responseJSON { response in
+                switch response.result {
+                case .success(let value):
+                    if let json = value as? [String: Any],
+                       let title = json["title"] as? String {
+                        responseData = title
+                    }
+                case .failure(let error):
+                    responseData = "Error: \(error.localizedDescription)"
+                }
+            }
     }
 }
 
